1e103e3610fbe59b2db4a9c4e14387bdb24aa737
your_username <your_email_address@example.com>
2022-06-28 15:57:37 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'drag-and-drop',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
